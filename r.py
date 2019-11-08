014250f2e716d4dc106d1e8d12fb11747d9ead55
supermomie <fakhredinedev3.0@gmail.com>
2019-11-08 02:36:23 +0100

    ajout d'un nombre manquant dans la liste et correction des resulats

--- r.py
+++ r.py
@@ -2,7 +2,7 @@ from math import *
 
 
 #liste des donnees
-ls = [4,5,5,7,8,10,10,11,15,40]
+ls = [4,5,5,7,8,9,10,10,11,15,40]
 
 
 #calcule la sommes de tous les element de la donnee
@@ -20,18 +20,18 @@ n_ls = []
 print("Calcule chaque element par la sommes des donnees diviser par le nombre total d'element le tout au carree \n")
 for i in ls:
     res= pow(i - (sum_ls/totol_elem), 2)
-    print("({} - {}) ^ 2 =".format(i, sum_ls/totol_elem), res)
+    print("({} - {}) ^ 2 =".format(i, round(sum_ls/totol_elem, 2)), round(res, 2))
     n_ls.append(res)
 
 print("\n")
 
 #compte la sommes des carres des ecarts
 total_sum_pow = sum(n_ls)
-print("Total des sommes des carres des ecart :", total_sum_pow)
+print("Total des sommes des carres des ecart :", round(total_sum_pow, 2))
 print()
 #calcule de la Variance -> calcule des somme des carrés des écarts par le nombre de degrés de liberté
 variance = 1/totol_elem * total_sum_pow
-print("Variance : 1 / {} * {} = {}".format(totol_elem,total_sum_pow,variance))
+print("Variance : 1 / {} * {} = {}".format(totol_elem,round(total_sum_pow,2),round(variance,2)))
 print()
 
 #calcule de l'ecart type : c'est la racine carree de la variance
